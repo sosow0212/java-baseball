4115e0dca3d869076c53416960934e8299f60aa5
sosow0212 <sosow0212@naver.com>
2022-11-25 18:10:57 +0900

    refactor : (Computer) 컴퓨터 넘버 생성하는 로직을 외부로 빼서 테스트코드 작성하기 쉽게 리팩토링 진행

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,7 +1,5 @@
 package baseball.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
@@ -9,8 +7,8 @@ public class Computer {
     private int strikeCount;
     private int ballCount;
 
-    public Computer() {
-        makeComputerNumbers();
+    public Computer(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
         this.strikeCount = 0;
         this.ballCount = 0;
     }
@@ -19,21 +17,10 @@ public class Computer {
         return computerNumbers;
     }
 
-    public void resetGame() {
-        makeComputerNumbers();
+    public void resetGame(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
         this.strikeCount = 0;
         this.ballCount = 0;
-    }
-
-    private void makeComputerNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber);
-            }
-        }
-        this.computerNumbers = numbers;
     }
 
     public int getStrikeCount() {
