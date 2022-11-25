14d246127f159a4972b633eca9e14d747f2079fd
sosow0212 <sosow0212@naver.com>
2022-11-25 16:34:42 +0900

    feat : (Computer) 게임 결과를 계산해주는 기능 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,59 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computerNumbers;
+    private int strikeCount;
+    private int ballCount;
+
+    public Computer(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public boolean isThreeStrike() {
+        if (this.strikeCount == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public void compareNumbers(List<Integer> userNumbers) {
+        initCountOfBallAndStrike();
+        getCountOfBallAndStrikeCount(userNumbers);
+    }
+
+    private void initCountOfBallAndStrike() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    private void getCountOfBallAndStrikeCount(List<Integer> userNumbers) {
+        for (int i = 0; i < userNumbers.size(); i++) {
+            calculateStrikeCount(userNumbers.get(i), computerNumbers.get(i));
+            calculateBallCount(userNumbers.get(i), computerNumbers.get(i));
+        }
+    }
+
+    private void calculateStrikeCount(int userNumber, int computerNumber) {
+        if (userNumber == computerNumber) {
+            strikeCount++;
+        }
+    }
+
+    private void calculateBallCount(int userNumber, int computerNumber) {
+        if (computerNumbers.contains(userNumber) && computerNumber != userNumber) {
+            ballCount++;
+        }
+    }
+}
