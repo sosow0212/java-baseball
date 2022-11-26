08c79d4a62135f4c124976e6ffe4cc8224ca9ce8
sosow0212 <sosow0212@naver.com>
2022-11-26 19:26:42 +0900

    feat : (GameException) 중복 숫자 예외 추가

--- src/main/java/baseball/exception/GameException.java
+++ src/main/java/baseball/exception/GameException.java
@@ -1,5 +1,9 @@
 package baseball.exception;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class GameException {
     private static final String ERROR = "[ERROR] ";
     private static final String USER_INPUT_ERROR = "입력은 1~9의 숫자 3개로 이뤄져야합니다.";
@@ -8,6 +12,7 @@ public class GameException {
     public void validateUserNumberInput(String input) {
         validateUserNumbersContainsOnlyNumbers(input, USER_INPUT_ERROR);
         validateUserNumbersPermittedSize(input, USER_INPUT_ERROR);
+        validateIsSame(input);
         validateUserNumbersPermittedRange(input, USER_INPUT_ERROR);
     }
 
@@ -17,7 +22,7 @@ public class GameException {
         validateNextGameStatusNumberRange(input, NEXT_GAME_INPUT_ERROR);
     }
 
-    public void validateUserNumbersContainsOnlyNumbers(String input, String message) {
+    private void validateUserNumbersContainsOnlyNumbers(String input, String message) {
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i))) {
                 exception(message);
@@ -25,19 +30,26 @@ public class GameException {
         }
     }
 
-    public void validateUserNumbersPermittedSize(String input, String message) {
+    private void validateIsSame(String input) {
+        Set<String> set = Arrays.stream(input.split("")).collect(Collectors.toSet());
+        if (set.size() != 3) {
+            exception("중복 안됨");
+        }
+    }
+
+    private void validateUserNumbersPermittedSize(String input, String message) {
         if (input.length() != 3) {
             exception(message);
         }
     }
 
-    public void validateNextGameStatusInputSize(String input, String message) {
+    private void validateNextGameStatusInputSize(String input, String message) {
         if (input.length() != 1) {
             exception(message);
         }
     }
 
-    public void validateUserNumbersPermittedRange(String input, String message) {
+    private void validateUserNumbersPermittedRange(String input, String message) {
         for (int i = 0; i < input.length(); i++) {
             int number = input.charAt(i) - '0';
             if (!(number >= 1 && number <= 9)) {
@@ -46,7 +58,7 @@ public class GameException {
         }
     }
 
-    public void validateNextGameStatusNumberRange(String input, String message) {
+    private void validateNextGameStatusNumberRange(String input, String message) {
         for (int i = 0; i < input.length(); i++) {
             int number = input.charAt(i) - '0';
             if (!(number >= 1 && number <= 2)) {
@@ -55,7 +67,7 @@ public class GameException {
         }
     }
 
-    public void exception(String message) {
+    private void exception(String message) {
         throw new IllegalArgumentException(ERROR + message);
     }
 }
